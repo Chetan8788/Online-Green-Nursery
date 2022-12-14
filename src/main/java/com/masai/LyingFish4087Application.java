82d7f118bd17444e5fa043afcc88f9017dd2daeb
indicate0 <indicate0@gmail.com>
2022-12-14 19:12:02 +0530

    first commit

---
+++ src/main/java/com/masai/LyingFish4087Application.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyingFish4087Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyingFish4087Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/masai/LyingFish4087ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyingFish4087ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
