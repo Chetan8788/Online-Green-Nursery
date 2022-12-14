755dec74715ac7592283f7c3a3c8bfcb6d126162
SudhanshuRoy <shudhanshukumarmuz@gmail.com>
2022-12-14 18:26:38 +0530

    added

---
+++ laying-fish-4087/src/main/java/com/masai/LayingFish4087Application.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayingFish4087Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayingFish4087Application.class, args);
+	}
+
+}
