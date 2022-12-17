8174c03ecf3a461654bdda1d72d6206a30cb86f1
SudhanshuRoy <shudhanshukumarmuz@gmail.com>
2022-12-17 17:56:23 +0530

    added

--- src/main/java/com/masai/service/CustomerService.java
+++ src/main/java/com/masai/service/CustomerService.java
@@ -4,5 +4,7 @@ import com.masai.exception.CustomerException;
 import com.masai.model.Customer;
 
 public interface CustomerService {
+	
  public Customer registerCustomer(Customer customer) throws CustomerException;
+ 
 }
